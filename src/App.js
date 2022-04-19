44439f8e0cc2513207bafe7fcab60fbb0f9ca7e6
Ian Connell <ian10gt@gmail.com>
2022-04-19 15:28:27 +0100

    Tracy's code in App.JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const [error, setError] = useState(null);
+  console.log(characters);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await fetch(
+          "https://www.breakingbadapi.com/api/characters?limit=10&offset=10"
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        setCharacters(data);
+      } catch (err) {
+        setError("Could not fetch data");
+        console.log(err.message);
+      }
+    };
+    fetchCharacters();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error && <p>{error}</p>}
+      <div>
+        <h1>Breaking bad Characters</h1>
+        {characters.map((character) => (
+          <div key={character.char_id}>
+            <h1>{character.name}</h1>
+            <h2>{character.birthday}</h2>
+            <h3>{character.occupation}</h3>
+            <img src={character.img} alt="breaking bad characters"></img>
+            <h5>{character.status}</h5>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
